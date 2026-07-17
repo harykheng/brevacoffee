c195d567f7291cb0f7a41c9c397cecdd86b2bff5
Haryanto <37305222+harykheng@users.noreply.github.com>
2026-07-17 12:33:27 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ const SUPABASE_URL = 'https://ftuxnxrbydnfrpdyisht.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0dXhueHJieWRuZnJwZHlpc2h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NjkzNTIsImV4cCI6MjA5ODA0NTM1Mn0.qK-9J90EoHO_c1VsU5BZ_KV4QbeoFP_1qRK-QMfCGnE';
 
 // Nomor WhatsApp admin (format: 62xxxxxxxxx, tanpa + atau spasi)
-const ADMIN_WHATSAPP = '62881025030603';
+const ADMIN_WHATSAPP = '1231231321321321321';
 
 // Nama toko (tampil di pesan WhatsApp)
 const STORE_NAME = 'Breva Coffee';
